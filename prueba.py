32f923a3f44e6446acd2f9095eb59edaf3c90b66
ErickLimAce <a01735902@tec.mx>
2025-04-24 17:15:40 -0600

    Corrección token_name

--- prueba.py
+++ prueba.py
@@ -5,4 +5,5 @@ input_text = "FORECAST PM25 AT zona_norte IN 2 HOURS IF WIND > 10 AND HUMIDITY <
 lexer = ForecastLexer(InputStream(input_text))
 
 for token in lexer.getAllTokens():
-    print(f"{token.text:<15} --> {token.type}")
+    token_name = lexer.symbolicNames[token.type]
+    print(f"{token.text:<15} --> {token_name}")
